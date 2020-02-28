610b86a850fe71b3116aaf6bd5a319ed193f3fd6
pokkst <pokkst@protonmail.com>
2020-02-27 20:01:50 -0600

    Upgrade gradle to 5.6.4 and use AddressUtil.kt in SimpleLedgerAddressTest.kt

--- app/src/test/java/com/bitcoin/merchant/app/SimpleLedgerAddressTest.kt
+++ app/src/test/java/com/bitcoin/merchant/app/SimpleLedgerAddressTest.kt
@@ -10,21 +10,21 @@ internal class SimpleLedgerAddressTest {
     @Test
     fun validateSlpAddr() {
         val address = "simpleledger:qqrxa0h9jqnc7v4wmj9ysetsp3y7w9l36utncg8up7"
-        val cashAddr = SlpAddress(MainNetParams.get(), address).toCashAddress()
+        val cashAddr = AddressUtil.fromSimpleLedgerAddress(address)
         assert(AddressUtil.isValidCashAddr(cashAddr))
     }
 
     @Test
     fun cashAddrToSlpAddr() {
         val address = "bitcoincash:qqrxa0h9jqnc7v4wmj9ysetsp3y7w9l36u8gnnjulq"
-        val slpAddr = SlpAddress.fromCashAddr(MainNetParams.get(), address).toString()
+        val slpAddr = AddressUtil.toSimpleLedgerAddress(address)
         assertEquals(slpAddr, "simpleledger:qqrxa0h9jqnc7v4wmj9ysetsp3y7w9l36utncg8up7")
     }
 
     @Test
     fun slpAddrToCashAddr() {
         val address = "simpleledger:qqrxa0h9jqnc7v4wmj9ysetsp3y7w9l36utncg8up7"
-        val cashAddr = SlpAddress(MainNetParams.get(), address).toCashAddress()
+        val cashAddr = AddressUtil.fromSimpleLedgerAddress(address)
         assertEquals(cashAddr, "bitcoincash:qqrxa0h9jqnc7v4wmj9ysetsp3y7w9l36u8gnnjulq")
     }
 }
